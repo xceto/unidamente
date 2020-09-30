a27d2a4f89bf55f74f2157a3505d9717ac7de0d9
CeTo <ctroncoso@i-med.cl>
2020-09-30 20:15:44 -0300

    from repo

--- app.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    var message = 'Prueba de nodejs! from MASTER\n',
-        version = 'NodeJS ' + process.versions.node + '\n',
-        response = [message, version].join('\n');
-    res.end(response);
-});
-server.listen();
