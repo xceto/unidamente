3311330c6a294378a71cb9d15307f5109de857c2
CeTo <ctroncoso@i-med.cl>
2020-09-30 19:49:18 -0300

    test master

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var server = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    var message = 'Prueba de nodejs!\n',
+    var message = 'Prueba de nodejs! from MASTER\n',
         version = 'NodeJS ' + process.versions.node + '\n',
         response = [message, version].join('\n');
     res.end(response);
